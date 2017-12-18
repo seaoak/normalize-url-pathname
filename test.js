f91eb7096a7bde0fb495ad36a37f79f6e0241b8d
Seaoak <seaoak2003@gmail.com>
2017-12-18 17:13:33 +0900

    Add test cases

--- test.js
+++ test.js
@@ -7,5 +7,11 @@ const assert = require('assert');
 console.log(typeof URL);
 console.log(typeof normalizer);
 
-assert.strictEqual(normalizer('/aaa'), '/aaa');
-assert.strictEqual(normalizer('/bbb/../../ccc/./../ddd/././eee'), '/ddd/eee');
+assert.strictEqual(normalizer('/'), '/'); // root
+assert.strictEqual(normalizer('/aaa'), '/aaa'); // simple file
+assert.strictEqual(normalizer('/aaa/'), '/aaa/'); // simple directory
+assert.strictEqual(normalizer('/aaa/./bbb'), '/aaa/bbb'); // simple "single-dot"
+assert.strictEqual(normalizer('/aaa/../bbb'), '/bbb'); // simple "double-dot"
+assert.strictEqual(normalizer('/aaa/././bbb'), '/aaa/bbb'); // a sequence of "single-dot"
+assert.strictEqual(normalizer('/aaa/bbb/ccc/../../ddd'), '/aaa/ddd'); // a sequence of "double-dot"
+assert.strictEqual(normalizer('/aaa/.././bbb/./ccc/././ddd/./.././eee/../../fff/./'), '/bbb/fff/'); // a mixed sequence of "single-dot" and "double-dot"
