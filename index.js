832747d90b1879690a3e1f1b4caca0d07cd48e96
Seaoak <seaoak2003@gmail.com>
2017-12-19 18:09:26 +0900

    First fully implementation

--- index.js
+++ index.js
@@ -7,11 +7,47 @@
     throw new Error('Sorry! This environment is not supported.');
   })();
 
+  function pipe() {
+    const funcList = Array.from(arguments);
+    const first = funcList.shift();
+    Object.freeze(funcList); // just in case
+    if (! first) throw new TypeError('too few arguments');
+    if (! first.apply) throw new TypeError('invalid argument');
+    return function pipeHelper() {
+      const args = Array.from(arguments);
+      return funcList.reduce((acc, func) => func(acc), first.apply(null, args));
+    };
+  }
+
+  function reduceConsecutiveSeparators(str) {
+    return str.replace(/\/{2,}/g, '/');
+  }
+
+  function normalizePercentEncoding(str) {
+    return str.replace(/%[\s\S]{0,2}/g, str => str.match(/^%[0-9a-fA-F]{2}$/) ? str.toUpperCase() : '\0');
+  }
+
+  function removeInvalidCharacter(str) {
+    return str.replace(/[^-._~a-zA-Z0-9%!$&''()*+,;=:@\/]/g, str => '\0'); // RFC3986
+  }
+
+  const cleaner = pipe(
+    str => str.normalize(),
+    removeInvalidCharacter,
+    normalizePercentEncoding,
+    reduceConsecutiveSeparators,
+    str => str.includes('\0') ? undefined : str);
+
   function normalizeUrlPathname(argPathname, argBaseUrl) {
     console.log('I\'m here!');
     const baseUrl = argBaseUrl || 'https://localhost/';
-    const url = new MyURL(argPathname, baseUrl);
-    return url.pathname;
+    if (! argPathname) return '/';
+    const cleaned = cleaner(argPathname);
+    if (! cleaned) return undefined;
+    const url = new MyURL(cleaned, baseUrl);
+    const result = url.pathname;
+    if (! result || result.includes('//')  || result.includes('/./') || result.includes('/../')) throw new Error('unexpected behavior of URL API: ' + url);
+    return result;
   }
 
   if (typeof module !== 'undefined') {
